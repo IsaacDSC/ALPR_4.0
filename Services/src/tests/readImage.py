2022740dc9bb9cbb297fd9b7f81ef5aceb4f8676
IsaacDSC <isaac8.silva@hotmail.com>
2022-04-10 11:53:15 -0300

    feat: create test model train identify plate

--- Services/src/tests/readImage.py
+++ Services/src/tests/readImage.py
@@ -1,16 +1,52 @@
 import cv2
 from datetime import datetime
 import easyocr
+from matplotlib import pyplot as plt
+import numpy as np
+import imutils
 
 
-imagem = cv2.imread('src/public/simulate/car01.png')
-cv2.imshow("Original", imagem)
 path_images = "src/public/images/test_%s.png" % (datetime.now())
-grayFrame = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
-(thresh, blackAndWhiteFrame) = cv2.threshold(
-    grayFrame, 127, 255, cv2.THRESH_BINARY)
 
-cv2.imwrite(path_images, blackAndWhiteFrame)
-reader = easyocr.Reader(['ch_sim','en']) # need to run only once to load model into memory
-result = reader.readtext(blackAndWhiteFrame)
+
+img = cv2.imread('src/public/simulate/car01.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+plt.imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
+
+bfilter = cv2.bilateralFilter(gray, 11, 17, 17) #Noise reduction
+edged = cv2.Canny(bfilter, 30, 200) #Edge detection
+plt.imshow(cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
+
+# cv2.imwrite(path_images, edged)
+
+keypoints = cv2.findContours(
+    edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(keypoints)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+
+location = None
+for contour in contours:
+    approx = cv2.approxPolyDP(contour, 10, True)
+    if len(approx) == 4:
+        location = approx
+        break
+
+mask = np.zeros(gray.shape, np.uint8)
+new_image = cv2.drawContours(mask, [location], 0,255, -1)
+new_image = cv2.bitwise_and(img, img, mask=mask)
+
+plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
+
+(x,y) = np.where(mask==255)
+(x1, y1) = (np.min(x), np.min(y))
+(x2, y2) = (np.max(x), np.max(y))
+cropped_image = gray[x1:x2+1, y1:y2+1]
+
+plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+
+reader = easyocr.Reader(['en'])
+result = reader.readtext(cropped_image)
 print(result)
+
+# cv2.imwrite(path_images, new_image)
+
